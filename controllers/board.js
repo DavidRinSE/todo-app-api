a22659bf0ed147f48057c5e765b77ef9cc05bb75
DavidRinSE <davidrichardsonin@gmail.com>
2020-02-09 11:02:28 -0600

    removed boards api call

--- controllers/board.js
+++ controllers/board.js
@@ -1,13 +1,38 @@
 const express = require("express");
 const router = express.Router();
-const db = require("../models")
+const db = require("../models");
+const Responses = require("./responses");
 
 const Board = db.Board
+const Task = db.Task
 
-router.get("/", (req, res) => {
-    res.json({
-        message: "/api/board get"
+router.get("/", async (req, res) => {
+    let where = null
+    
+    if(req.query.username && typeof req.query.username === 'string'){
+        where = {
+            username: req.query.username
+        }
+    } else if(req.query.id && !isNaN(parseInt(req.query.id))){
+        where = {
+            id: parseInt(req.query.id)
+        }
+    } else {
+        return res.status(400).send(JSON.stringify(Responses.BadRequest("username or id", "string or number")))
+    }
+
+    let boards = await Board.findAll({
+        where,
+        include: [Task],
+        limit: req.query.limit || 100,
+        offset: req.query.offset || 0,
+        order: [["createdAt", "DESC"]],
     })
+    if (boards.length < 1){
+        return res.status(404).send(JSON.stringify(Responses.NotFound("board")))
+    }
+    return res.json({boards: boards, statusCode:200})
+
 })
 
 router.post("/", (req, res) => {
